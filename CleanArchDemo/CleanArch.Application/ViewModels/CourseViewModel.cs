65fa43e300518a8822fe06fba76e5e99fab08d59
azeem <ad@watarpartners.com>
2022-04-07 15:35:55 +0500

    Commit course service and repository interfaces and course view model

---
+++ CleanArchDemo/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.ViewModels
+{
+	public class CourseViewModel
+	{
+		public IEnumerable<Course> GetCourses { get; set; }
+	}
+}
---
+++ CleanArchDemo/CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Interfaces
+{
+	public interface ICourseRepository
+	{
+
+		public IEnumerable<Course> GetCourses { get; set; }
+	}
+}
