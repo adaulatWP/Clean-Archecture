a4b8a5fcbb0ab47c491698abd1956d20a4f2697b
azeem <ad@watarpartners.com>
2022-04-07 15:51:31 +0500

    4/7/2022 changes

---
+++ CleanArchDemo/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+	public class CourseService : ICourseService
+	{
+		private ICourseRepository _icourseRepository;
+
+		public CourseService(ICourseRepository icourseRepository)
+		{
+			 _icourseRepository = icourseRepository;
+		}
+		public IEnumerable<CourseViewModel> GetCourses()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CleanArchDemo/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+	public class CourseRepository : ICourseRepository
+	{
+		private UniversityDbContext _context;
+
+
+		public CourseRepository(UniversityDbContext context)
+		{
+			 _context = context;
+		}
+		public IEnumerable<Course> GetCourses 
+		 {
+			get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+		}
+	}
+}
